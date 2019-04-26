6b8e38154b59ce711c292787e65d76af9ef60c86
drj613 <drj613@gmail.com>
2019-04-26 16:09:17 -0700

    pulled input-grabber back

---
+++ get_input.rb
@@ -0,0 +1,14 @@
+puts "Input environment"
+env = gets.chomp
+
+puts "Input login email"
+email = gets.chomp
+
+puts "Input login password"
+password = gets.chomp
+
+puts "ENV: #{env} \n
+EMAIL: #{email} \n
+PASSWORD: #{password}"
+
+sleep 2
